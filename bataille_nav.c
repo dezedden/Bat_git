6e10834286d831e1d08ec51e16e1d8345f95eec3
dezedden <fahtoure96@yahoo.fr>
2016-12-05 11:52:55 +0100

    ma premiere version du jeu qui fonctionne.

--- bataille_nav.c
+++ bataille_nav.c
@@ -5,6 +5,9 @@ void affiche_tab();
 int placer_bateau();
 char intialiser_tableau();
 int Tab[10][10];
+int Tab_ordi[10][10];
+char intialiser_tableau_ordi();
+void affiche_tab_ordi();
 char tirer();
 char tirer_ordi();
 int placer_bateau_ordi();
@@ -50,8 +53,9 @@ char tirer()
 	  j=n-1;
 	}while(j<0 || j>9);//tant
 	bon=0;
-	if(Tab[i][j]=='0')
+	if(Tab_ordi[i][j]=='0')
 	{
+         Tab_ordi[i][j]='T';
          Tab[i][j]='T';
          affiche_tab();
          bon=1;
@@ -63,6 +67,7 @@ char tirer()
 	}
 	else
 	{
+        Tab_ordi[i][j]='R';
         Tab[i][j]='R';
         affiche_tab();
         printf("RATE, C'est au tour de l'ordianteur\n\n ");
@@ -94,11 +99,11 @@ int placer_bateau_ordi()
     { bon=1;
       for (h=0;h<l;h++) // Ici on parcourt d'abord la colonne pour savoir s'il n'y a pas d'autres valeurs"0"
       {
-        if(Tab[i+h][j]=='0' || (i+h)>=10) bon=0;
+        if(Tab_ordi[i+h][j]=='0' || (i+h)>=10) bon=0;
       }
       if(bon)
       {
-        for (k=0;k<l;k++) Tab[i+k][j]='0';
+        for (k=0;k<l;k++) Tab_ordi[i+k][j]='0';
         l+=1;
       }
 
@@ -107,11 +112,11 @@ int placer_bateau_ordi()
     { bon=1;
       for (h=0;h<l;h++)
       {
-        if(Tab[i][j+h]=='0' || (j+h)>=10) bon=0;
+        if(Tab_ordi[i][j+h]=='0' || (j+h)>=10) bon=0;
       }
       if(bon)
       {
-        for (k=0;k<l;k++) Tab[i][j+k]='0';
+        for (k=0;k<l;k++) Tab_ordi[i][j+k]='0';
         l+=1;
       }
       else u--;
@@ -119,7 +124,7 @@ int placer_bateau_ordi()
     else u--;
     }
    }
-   tirer();
+   //tirer();
 }
 
 int placer_bateau()
@@ -183,6 +188,7 @@ int placer_bateau()
     affiche_tab();
    }
    printf("L ORDINATEUR VIENT DE PLACER SES BATEAUX   LA PARTIE PEUT COMMENCER :) \n\nVEUILLEZ TIRER SVP\n\n");
+   tirer();
 }
 
 
@@ -199,6 +205,34 @@ int placer_bateau()
     		}
  		}
  }
+ char initialiser_tableau_ordi()
+ {
+ 	int i;
+ 	int j;
+
+    	for(i=0;i<10;i++)
+    	{
+    		for(j=0;j<10;j++)
+    		{
+    			Tab_ordi[i][j]='1';
+    		}
+ 		}
+ }
+
+void affiche_tab_ordi()
+{
+	int i,j;
+	printf("   1     2     3     4     5     6     7     8     9    10\n");
+	for(i=0;i<10;i++){
+		for(printf("%c",'A'+i),j=0;j<10;j++){
+
+			printf( "  %c   ",Tab_ordi[i][j]);
+
+		}
+		printf("\n");
+	}
+
+}
 
 void affiche_tab()
 {
@@ -218,7 +252,10 @@ int main ()
 {
     srand(time(NULL));
     intialiser_tableau();
+    initialiser_tableau_ordi();
+    //affiche_tab();
     placer_bateau();
     placer_bateau_ordi();
+    //affiche_tab_ordi();
 
 }
